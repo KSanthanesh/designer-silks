91e4cf99d738e1836140b531564569eb5cc9c9a6
KSanthanesh <santhanesh_2004@yahoo.com>
2021-12-01 17:09:00 +0000

    Add product model for product and category

--- products/models.py
+++ products/models.py
@@ -1,3 +1,31 @@
+""" Product models database """
+
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """ Saree Category"""
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, blank=True)
+
+    def __str__(self):
+        return str(self.name)
+
+    def get_friendly_name(self):
+        """ Friendly name for the Product Display"""
+        return str(self.friendly_name)
+
+
+class Product(models.Model):
+    """Product details database"""
+
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)  # noqa: E501
+    sku = models.CharField(max_length=254, blank=True)
+    name = models.CharField(max_length=254)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)  # noqa: E501
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.name)
