2c1931e05f35626b67934a9ed238f4bc37b99dd4
KSanthanesh <santhanesh_2004@yahoo.com>
2021-12-06 17:44:22 +0000

    Sorting Sarees in products.html page and add anchor tag for product details page

--- products/views.py
+++ products/views.py
@@ -28,7 +28,7 @@ def all_products(request):
                 direction = request.GET['direction']
                 if direction == 'desc':
                     sortkey = f'-{sortkey}'
-                products = products.order_by(sortkey)
+            products = products.order_by(sortkey)
 
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
@@ -44,7 +44,7 @@ def all_products(request):
             queries = Q(name__icontains=query) | Q(description__icontains=query)  # noqa: E501
             products = products.filter(queries)
 
-        current_sorting = f'{sort}_{direction}'
+    current_sorting = f'{sort}_{direction}'
 
     context = {
         'products': products,
