d1c8d831d378867f6e944d35a868c957adb44298
KSanthanesh <santhanesh_2004@yahoo.com>
2021-12-13 17:51:13 +0000

    Add apps, signals and forms.py for checkout

---
+++ checkout/forms.py
@@ -0,0 +1,38 @@
+from django import forms
+from checkout.models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('first_name', 'last_name', 'email', 'phone_number',
+                  'house_number', 'address_line1', 'address_line2',
+                  'county_or_city', 'postcode', 'country',)
+
+    def __init__(self, *args, **kwargs):
+        """ Add placeholders and classes, remove re-generated lables and
+        set autofocus
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'first_name': 'First Name',
+            'last_name': 'Last Name',
+            'email': 'Email',
+            'phone_number': 'Phone Number',
+            'house_number': 'House Number',
+            'address_line1': 'Street Name',
+            'address_line2': 'Street Name(Optional)',
+            'country_or_city': 'County or City',
+            'postcode': 'Postcode(Optional)',
+            'country': 'Please Select Country',
+        }
+
+        self.fields['first_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]}*'
+            else:
+                placeholder = placeholders[field]
+        self.fields[field].widget.attrs['placeholder'] = placeholder
+        self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+        self.fields[field].label = False
