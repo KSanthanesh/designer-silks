34be34c38cbb2d6e1140d40ac55e384c3e039f35
KSanthanesh <santhanesh_2004@yahoo.com>
2021-12-15 18:16:21 +0000

    Add js and style for stripe payment system

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -4,5 +4,23 @@
     CSS from here: 
     https://stripe.com/docs/stripe-js
 */
-var stripe_public_key = $('id_stripe_public_key').text.slice(1, -1);
-var client_secret = $('id_client_secret').text.slice(1, -1);
+let stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+let client_secret = $('#id_client_secret').text().slice(1, -1);
+let stripe = Stripe(stripe_public_key);
+let elements = stripe.elements('card');
+
+let style = {
+    base: {
+        color: '#000',
+        fontSize: '16px',
+        fontFamily: 'poppins',
+        '::placeholder': {
+            color: '#aab7c4'
+        }
+    },
+    invalid: {
+        color: '#dc3545',
+        iconColor: '#dc3545'
+    }
+}
+card.mount('#card-element', {style: style});
