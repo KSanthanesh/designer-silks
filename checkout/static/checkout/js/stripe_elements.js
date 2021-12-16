3feb8593bdb2bc08749f70f35ee850a689e244d7
KSanthanesh <santhanesh_2004@yahoo.com>
2021-12-16 11:17:19 +0000

    Add basic stripe checkout functionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -4,9 +4,9 @@
     CSS from here: 
     https://stripe.com/docs/stripe-js
 */
-let stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-let client_secret = $('#id_client_secret').text().slice(1, -1);
-let stripe = Stripe(stripe_public_key);
+let stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+let clientSecret = $('#id_client_secret').text().slice(1, -1);
+let stripe = Stripe(stripePublicKey);
 let elements = stripe.elements();
 
 let style = {
@@ -41,3 +41,34 @@ card.addEventListener('change', function(event) {
         errorElement.textContent = '';
     }
 })
+
+// Create a token or display an error when the form is submitted.
+var form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  card.update({ 'disabled': true})
+  $('#submit-btn').attr('disabled', true)
+
+  stripe.confirmCardPayment(clientSecret, {
+    payment_method: {
+        card: card,
+    }
+}).then(function(result) {
+    if (result.error) {
+        var errorElement = document.getElementById('card-errors');
+        var html = `
+            <span class="icon" role="alert">
+            <i class="fas fa-times"></i>
+            </span>
+            <span>${result.error.message}</span>`;
+        $(errorElement).html(html);
+        card.update({ 'disabled': false});
+        $('#submit-btn').attr('disabled', false);
+    } else {
+        if (result.paymentIntent.status === 'succeeded') {
+            form.submit();
+        }
+    }
+});
+});
+
