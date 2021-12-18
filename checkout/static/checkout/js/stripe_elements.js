78591bf964fe6c78365895272ef9f10721598242
KSanthanesh <santhanesh_2004@yahoo.com>
2021-12-18 21:15:59 +0000

    Add cache checkout form in payment intent

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -45,33 +45,73 @@ card.addEventListener('change', function(event) {
 // Create a token or display an error when the form is submitted.
 var form = document.getElementById('payment-form');
 form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  card.update({ 'disabled': true})
-  $('#submit-btn').attr('disabled', true)
-  $('#payment-form').fadeToggle(100);
-  $('#loading-screen').fadeToggle(100);
-  stripe.confirmCardPayment(clientSecret, {
-    payment_method: {
-        card: card,
-    }
-}).then(function(result) {
-    if (result.error) {
-        var errorElement = document.getElementById('card-errors');
-        var html = `
-            <span class="icon" role="alert">
-            <i class="fas fa-times"></i>
-            </span>
-            <span>${result.error.message}</span>`;
-        $(errorElement).html(html);
-        $('#payment-form').fadeToggle(100);
-        $('#loading-screen').fadeToggle(100);
-        card.update({ 'disabled': false});
-        $('#submit-btn').attr('disabled', false);
-    } else {
-        if (result.paymentIntent.status === 'succeeded') {
-            form.submit();
-        }
-    }
-});
+    event.preventDefault();
+    card.update({ 'disabled': true})
+    $('#submit-btn').attr('disabled', true)
+    $('#payment-form').fadeToggle(100);
+    $('#loading-screen').fadeToggle(100);
+
+    let saveInfo = Boolean($('#id-save-info').attr('checked'));
+    // Using the csrf_token from the form
+    let csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    let postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save_info': saveInfo,
+    };
+    let url = '/checkout/cache_checkout_data/';
+
+    $.post(url, postData).done(function() {
+        stripe.confirmCardPayment(clientSecret, {
+            payment_method: {
+                card: card,
+                billing_details: {
+                    name: $.trim(form.first_name.value) +''+ $.trim(form.last_name.value),
+                    email:$.trim(form.email.value),
+                    phone_number:$.trim(form.phone_number.value),
+                    address: {
+                        house_number: $.trim(form.house_number.value),
+                        line1: $.trim(form.address_line1.value),
+                        line2: $.trim(form.address_line2.value),
+                        county: $.trim(form.county_or_city.value),
+                        country: $.trim(form.country.value),
+                    }
+                }
+            },
+            shipping: {
+                    name: $.trim(form.first_name.value) +''+ $.trim(form.last_name.value),
+                    phone_number:$.trim(form.phone_number.value),
+                    address: {
+                        house_number: $.trim(form.house_number.value),
+                        line1: $.trim(form.address_line1.value),
+                        line2: $.trim(form.address_line2.value),
+                        county: $.trim(form.county_or_city.value),
+                        postcode: $.trim(form.postcode.value),
+                        country: $.trim(form.country.value),
+                    }
+                }
+        }).then(function(result) {
+            if (result.error) {
+                var errorElement = document.getElementById('card-errors');
+                var html = `
+                    <span class="icon" role="alert">
+                    <i class="fas fa-times"></i>
+                    </span>
+                    <span>${result.error.message}</span>`;
+                $(errorElement).html(html);
+                $('#payment-form').fadeToggle(100);
+                $('#loading-screen').fadeToggle(100);
+                card.update({ 'disabled': false});
+                $('#submit-btn').attr('disabled', false);
+            } else {
+                if (result.paymentIntent.status === 'succeeded') {
+                    form.submit();
+                }
+            }
+        });
+    }).fail(function() {
+        // just reload the page, the error will be in django messages
+        location.reload();
+    }) 
 });
 
--- checkout/views.py
+++ checkout/views.py
@@ -1,15 +1,34 @@
 """ Checkout views.py """
 
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
 import stripe
+import json
 
 from checkout.forms import OrderForm
 from products.models import Product
 from cart.contexts import cart_contents
 from .models import Order, OrderLineItem
+
+
+@require_POST
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'cart': json.dumps(request.session.get('cart', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, 'Sorry, Your payment cannot be processed \
+            right now. Please try again later')
+        return HttpResponse(content=e, status=400)
 
 
 def checkout(request):
@@ -79,7 +98,7 @@ def checkout(request):
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing. \
-            Did you forget to set it in your environment?')
+            Check if you have set it in your environment?')
 
     context = {
         'order_form': order_form,
