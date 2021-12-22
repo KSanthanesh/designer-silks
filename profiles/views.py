1fe302e38ec3a82fdc17d6c559bb40ec613d01c7
KSanthanesh <santhanesh_2004@yahoo.com>
2021-12-22 21:57:01 +0000

    Add style for allauth pages and add views for userprofile

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,15 @@
 """ Profiles views """
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from profiles.models import UserProfile
 
 
 def profile(request):
     """ Display the User details, order and payment confirmation"""
+    profile = get_object_or_404(UserProfile, user=request.user)
 
-    return render(request, 'profiles/profile.html')
+    context = {
+        'profile': profile,
+    }
+
+    return render(request, 'profiles/profile.html', context)
