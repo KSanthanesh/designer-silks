01dc43715b184ad06493f327cce1b7a9665bff3b
KSanthanesh <santhanesh_2004@yahoo.com>
2021-12-23 10:51:44 +0000

    Add Userprofile forms, views and templates

---
+++ profiles/forms.py
@@ -0,0 +1,38 @@
+""" Profiles form """
+
+from django import forms
+from profiles.models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    """ for User details"""
+    class Meta:
+        """ for personal details, address and order confirmation"""
+        model = UserProfile
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        """ Add placeholders and classes, remove re-generated lables and
+        set autofocus
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'default_phone_number': 'Phone Number',
+            'default_house_number': 'House Number',
+            'default_address_line1': 'Street Name',
+            'default_address_line2': 'Street Name(Optional)',
+            'default_county_or_city': 'County or City',
+            'default_postcode': 'Postcode(Optional)',
+        }
+
+        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'default_country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]}*'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = (
+                'border-black rounded-0 profile-input')
+            self.fields[field].label = False
--- profiles/views.py
+++ profiles/views.py
@@ -1,15 +1,31 @@
 """ Profiles views """
 
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+
 from profiles.models import UserProfile
+from profiles.forms import UserProfileForm
 
 
 def profile(request):
     """ Display the User details, order and payment confirmation"""
-    profile = get_object_or_404(UserProfile, user=request.user)
+
+    profileuser = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.Post, instance=profileuser)
+
+        if form.is_valid:
+            form.save()
+            messages.success(request, "Profiles Updated Successfully.")
+
+    form = UserProfileForm(instance=profileuser)
+    orders = profileuser.orders.all()
 
     context = {
-        'profile': profile,
+        'form': form,
+        'orders': orders,
+        'on_profile_page': True,
     }
 
     return render(request, 'profiles/profile.html', context)
