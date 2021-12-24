18838672c55a8b12289885b2ca576e3410c42058
KSanthanesh <santhanesh_2004@yahoo.com>
2021-12-24 22:03:52 +0000

    Add 002 Profiles email confirmation completed

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -3,6 +3,9 @@
 import json
 import time
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
 
 from checkout.models import Order, OrderLineItem
 from products.models import Product
@@ -14,6 +17,25 @@ class StripeWH_Handler:
 
     def __init__(self, request):
         self.request = request
+
+    def _send_confirmation_email(self, order):
+        """ Confirmation email send to the user """
+        customer_email = order.email
+        subject = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_subject.txt',
+            {'order': order}
+        )
+        body = render_to_string(
+            'checkout/confirmation_emails/confirmation_email_body.txt',
+            {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL}
+        )
+
+        send_mail(
+            subject,
+            body,
+            settings.DEFAULT_FROM_EMAIL,
+            [customer_email]
+        )
 
     def handle_event(self, event):
         """
@@ -78,6 +100,7 @@ class StripeWH_Handler:
                 attempt += 1
                 time.sleep(1)
         if order_exists:
+            self._send_confirmation_email(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
                 status=200
@@ -114,7 +137,8 @@ class StripeWH_Handler:
                 return HttpResponse(
                     content=f'Webhook received: {event["type"]} | \
                         Error: {e}', status=500)
-
+                        
+        self._send_confirmation_email(order)
         return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Created Order in Webhook',
                 status=200
