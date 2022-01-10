952fef4705dc3993456c893f2dfe258c2ebca93b
KSanthanesh <santhanesh_2004@yahoo.com>
2022-01-10 16:18:44 +0000

    Update settings.py

--- designer_silks/settings.py
+++ designer_silks/settings.py
@@ -195,7 +195,7 @@ if 'USE_AWS' in os.environ:
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
-	
+
     # Bucket config
     AWS_STORAGE_BUCKET_NAME = 'designer-silks'
     AWS_S3_REGION_NAME = 'eu-west-1'
