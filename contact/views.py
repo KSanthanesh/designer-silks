61f5606bab361fda39767d7161970e1aa856508b
KSanthanesh <santhanesh_2004@yahoo.com>
2022-01-11 11:59:22 +0000

    Update contact views

--- contact/views.py
+++ contact/views.py
@@ -1,11 +1,8 @@
 """ Views for contact page"""
 
-from django.shortcuts import render, redirect, reverse
-from django.http import HttpResponseRedirect
-from .models import Contact
-from .forms import ContactForm
+from django.shortcuts import render
 from django.contrib import messages
-
+from .forms import ContactForm
 
 
 def contacts(request):
@@ -13,22 +10,20 @@ def contacts(request):
     # contact = get_object_or_404(Contact)
     if request.method == "POST":
         form = ContactForm(request.POST)
-        if form.is_valid():
-            form.save()
-            name = request.POST['name']
-            if name == "guest":
-                messages.success(request, 'Thanks for Subscribing Newletter')
-                return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-            else:
-                messages.success(request, 'Thanks for Contact Us!')
-                
-                
-            
+        form.save()
+        # if form.is_valid():
+        #     form.save()
+        #     name = request.POST['name']
+        #     if name == "guest":
+        #         messages.success(request, 'Thanks for Subscribing Newletter')
+        #         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    # else:
+        # messages.success(request, 'Thanks for Contact Us!')
 
     form = ContactForm()
     # context = {
     #     'form': form,
     #     'contact': contact,
     # }
+    messages.success(request, 'Thanks for Contact Us!')
     return render(request, 'contact/contact.html')
-    
