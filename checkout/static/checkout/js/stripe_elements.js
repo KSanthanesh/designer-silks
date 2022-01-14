456782ef23bcde9afc6ed0fa32e6f2c8ff129cbf
KSanthanesh <santhanesh_2004@yahoo.com>
2022-01-14 16:53:21 +0000

    Modify first name to full name in stripe_elements.js

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -66,7 +66,7 @@ form.addEventListener('submit', function(ev) {
             payment_method: {
                 card: card,
                 billing_details: {
-                    name: $.trim(form.first_name.value) +''+ $.trim(form.last_name.value),
+                    name: $.trim(form.full_name.value),
                     email: $.trim(form.email.value),
                     phone: $.trim(form.phone_number.value),
                     address: {
@@ -78,7 +78,7 @@ form.addEventListener('submit', function(ev) {
                 }
             },
             shipping: {
-                    name: $.trim(form.first_name.value)+' '+ $.trim(form.last_name.value),
+                    name: $.trim(form.full_name.value),
                     phone: $.trim(form.phone_number.value),
                     address: {
                         line1: $.trim(form.address_line1.value),
